8bfc1109e789cf24b7729081b2c7ab0d7f35d2d9
LenniEmilMarks <lm.student@guest.um.si>
2023-04-13 13:09:03 +0200

    update of files

--- client/src/App.js
+++ client/src/App.js
@@ -2,50 +2,51 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   function fizzbuzz() {
+    function containsSpecialChars(str) {
+      const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+      return specialChars.test(str);
+    }
     var number1 = document.getElementById("number1").value;
     var number2 = document.getElementById("number2").value;
     var number1 = parseInt(number1);
     var number2 = parseInt(number2);
     let text = "";
+    if (containsSpecialChars(number1) || containsSpecialChars(number2)) {
+      text = "<p id='error'>Error: You can't enter special Characters!</p>";
+    }
+    else {
     for (let i = number1; i <= number2; i++) {
-      if (i % 15 == 0) var result = i + " FizzBuzz";
-      else if (i % 3 == 0) var result = i + " Fizz";
-      else if (i % 5 == 0) var result = i + " Buzz";
-      else var result = i;
+      if (i % 15 == 0)
+        var result = "<p id='fizzbuzz'>" + i + " FizzBuzz" + "</p>";
+      else if (i % 3 == 0)
+        var result = "<p id='fizz'>" + i + " " + "Fizz" + "</p>";
+      else if (i % 5 == 0)
+        var result = "<p id='buzz'>" + i + " " + "Buzz" + "</p>";
+      else var result = i + "<br/>";
 
-      text += result + "<br/>";
-    };
-
+      text += result;
+    }
+  }
     document.getElementById("output").innerHTML = text;
-
-};
+  }
   return (
-  <div>
-    <div className="container">
-    <h1>{!data ? "Loading..." : data}</h1>
-      <form>
-        <input type="number" name="number" id="number1" />
-        <input type="number" name="number" id="number2" />
-        <button type="button" id="submit" onClick={fizzbuzz}>Submit</button>
-      </form>
+    <div id="wrapper">
+      <div className="container">
+        <h1>Fizzbuzz App!</h1>
+        <form>
+          <input  type="number" min="0" name="number" id="number1" />
+          <input  type="number" min="0" name="number" id="number2" />
+          <button type="button" id="submit" onClick={fizzbuzz}>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="container2">
+        <p id="output"></p>
+      </div>
     </div>
-
-    <div className="container2">
-      <p id="output"></p>
-    </div>
-
-  </div>
   );
 }
 
 export default App;
-
